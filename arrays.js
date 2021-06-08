a605138df7125d2cc4460c4d8476764c6998b757
Savannah Simonis <savannah.simonis@x-center.com>
2021-06-08 08:07:46 +0200

    Huiswerk gemaakt

--- arrays.js
+++ arrays.js
@@ -2,17 +2,22 @@
 // 1. Declareer een variabele en sla daar een array met vier jaartallen (zoals 2021) in op. Log dit in de terminal.
 // ==========================================
 
+const years = ["2018", "2019", "2020", "2021"];
+console.log(years);
 
 // ==========================================
-// 2. Declareer een variabele en sla daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
+// 2. Declareer een variabele en slnodemona daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
 // ==========================================
 
+const hobbies = ["zingen", "gongfu thee", "lezen"];
+console.log(hobbies);
 
 // ==========================================
 // 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
 // 3b. Zorg er nu voor dat je de eerste waarde uit die array in de terminal logt             // geeft 3
 // ==========================================
-
+const waarden = [3, 4, 5, 7, 2];
+console.log(waarden[0]);
 
 // ==========================================
 // 4a. Declareer een variabele met daarin een array met de waardes groen, geel, rood, paars, blauw en oranje.
@@ -20,7 +25,9 @@
 // 4b. Zorg ervoor dat je de vijfde waarde uit bovenstaande array in de terminal logt       // geeft blauw
 // 4c. Zorg ervoor dat je de tweede waarde uit bovenstaande array in de terminal logt       // geeft geel
 // ==========================================
-
+const colors = ["groen", "geel", "rood","paars","blauw","orange"];
+console.log(colors[4]);
+console.log(colors[1]);
 
 // ==========================================
 // 5a. Declareer een variabele met daarin een array met de waardes 21, 22, 23, 25, 25 en log dit in de terminal
@@ -28,6 +35,9 @@
 // 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
 
+let values = [21, 22, 23, 25, 25];
+values[3] = 24;
+console.log(values);
 
 // ==========================================
 // 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
@@ -35,3 +45,8 @@
 // 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+
+let ingredients = ["bladerdeeg","knoflook","spinazie"];
+ingredients[0] = "lasagne bladen";
+console.log(ingredients);
+console.log(ingredients.length);
