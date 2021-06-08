daa1c07662ece7abff9272927e8b0073f062d73a
Savannah Simonis <savannah.simonis@x-center.com>
2021-06-08 08:20:59 +0200

    Tweede huiswerk opdracht gemaakt

--- objects.js
+++ objects.js
@@ -2,11 +2,18 @@
 // Maak een object met daarin jouw voornaam (string), achternaam (string), leeftijd (getal) en geboortedatum (date object). De datum hoeft niet geformat te worden.
 // Log dit in de terminal.
 
+const sav = {
+  firstName: "Savannah",
+  lastName: "Simonis",
+  age: 30,
+  dateOfBirth: "31-01-1991"
+}
 
 // ===  Opdracht 2 ===
 // Zorg ervoor dat de volgende string in de terminal wordt gelogd:
 // "Hoi! Ik ben <voornaam>, ik ben <leeftijd> jaar oud."
 
+console.log("Hoi, Ik ben "+sav.firstName+", ik ben " + sav.age+ " jaar oud.");
 
 // === Opdracht 3: bekijk het volgende object ===
 
@@ -21,17 +28,27 @@ const product = {
 
 // 3a. Log de naam van de serie in de terminal.
 
+console.log(product.productName);
+
 // 3b. Overschrijf de oude waarde van de property color ("creme") naar "roze". Log het object in de terminal om te checken of het gelukt is.
+
+product.color = "roze";
+console.log(product.color);
 
 // 3c. Log de tweede materiaalsoort ("kunststof") in de terminal
 
+console.log(product.material[1]);
+
 // 1d. Log de afmetingen (b-h-d) in de terminal
 
+console.log(product["b-h-d"]);
 
 // === Opdracht 4 ===
 // Declareer een variabele met daarin een string die het product beschrijft. Gebruik hiervoor de properties productName en material uit het object.
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
+
+console.log("De "+product.productName + " is gemaakt van " + product.material[0] + " en " + product.material[1]);
 
 
 
